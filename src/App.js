db503d0da97b75602e393ecfd6d604d44d000cfe
NotTayyy <robertantoine99@gmail.com>
2023-01-12 08:46:13 -0500

    fixed email isue need to fig why it broke

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
               </div>
             <br></br>
               <h1>Username: {user.username}</h1>
-              <h1>E-Mail: { "Nothing Found!" || user.email}</h1>
+              <h1>E-Mail: { user.email}</h1>
               {user.dateCreated ? (<p>Date Created: {user.dateCreated}</p> ) : (<br/>)}
               <button onClick={() => removeUser(user._id)}>DELETE USER</button>
             </div>
